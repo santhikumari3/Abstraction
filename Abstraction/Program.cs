e09d1e8f1b6b440ee00324c240edb2ea4e39aa37
syegireddi@turingsystems.com <syegireddi@turingsystems.com>
2022-02-21 14:33:10 +0530

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class Shape
+    {
+        public abstract float area();
+    }
+    class Square: Shape
+    {
+        private float side;
+        public Square(float x)
+        {
+            side = x;
+        }
+        public override float area()
+        {
+            Console.Write(" Area of square:");
+            return (side * side);
+         
+        }
+    }
+    class GFI
+    {
+        static void Main(string[] args)
+        {
+            Shape sh = new Square(4);
+            float result = sh.area();
+            Console.WriteLine("{0}",result);
+        }
+    }
+}
